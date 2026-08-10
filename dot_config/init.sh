9d9bed3fe3acafdbfa7246efe0de04c40aa5cdbf
yu-hasebe <yuhasebe0821@gmail.com>
2026-08-10 09:12:35 +0900

    chore: nudge

--- dot_config/init.sh
+++ dot_config/init.sh
@@ -37,10 +37,30 @@ persona() {
 	esac
 }
 
+nudge() {
+	local _result _pane
+	trap 'printf "\nnudge: stopped.\n"; return 0' INT TERM
+
+	printf "nudge: watching for agents waiting for input (Ctrl+C to stop)...\n"
+
+	while true; do
+		_result=$(herdr agent list 2>/dev/null) || {
+			sleep 2
+			continue
+		}
+
+		while IFS= read -r _pane; do
+			herdr pane send-keys "$_pane" Enter 2>/dev/null
+		done < <(printf '%s' "$_result" | jq -r '.result.agents[] | select(.agent_status | IN("idle", "running", "working") | not) | .pane_id')
+
+		sleep 1
+	done
+}
+
 _init_theme() {
 	local _dir="${XDG_CONFIG_HOME:-$HOME/.config}/starship"
 	local -a _themes=("$_dir"/*.toml)
-	export STARSHIP_CONFIG="${_themes[$((RANDOM % ${#_themes[@]}))]}"
+	export STARSHIP_CONFIG="${_themes[$((RANDOM % ${#_themes[@]} + 1))]}"
 }
 _init_theme
 unset -f _init_theme
