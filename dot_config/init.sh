f5f4f5ccda5031dd39eb32d6e37da2c5cbf21c2b
yu-hasebe <yuhasebe0821@gmail.com>
2026-08-10 21:43:37 +0900

    chore: message

--- dot_config/init.sh
+++ dot_config/init.sh
@@ -2,11 +2,16 @@
 
 export PATH="$HOME/.local/bin:$PATH"
 
+_msg() {
+	local _icon="$1" _name="$2" _text="$3"
+	printf "\033[1m[%s %s]\033[0m ▶ %s\n" "$_icon" "$_name" "$_text"
+}
+
 nudge() {
 	local _result _pane
-	trap 'printf "\nnudge: stopped.\n"; return 0' INT TERM
+	trap 'printf "\n"; _msg 👻 nudge "stopped. 🌙"; return 0' INT TERM
 
-	printf "nudge: watching for agents waiting for input (Ctrl+C to stop)...\n"
+	_msg 👻 nudge "watching for agents waiting for input (Ctrl+C to stop)..."
 
 	while true; do
 		_result=$(herdr agent list 2>/dev/null) || {
